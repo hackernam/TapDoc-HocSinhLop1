e8dd9e0d05a0b6e98b2f2a75fed7b28c30eeb1c1
qwert1650 <hongoctuan.it@gmail.com>
2015-12-09 20:04:06 +0700

    chinh sua nho ko anh huong ji het (Tuan)
    chinh sủa nho ko anh hương

--- Source-code/include/inc-dsbaihoc.php
+++ Source-code/include/inc-dsbaihoc.php
@@ -1,9 +1,19 @@
             <div class="content">
             <div class="header-clear-large"></div>
             <!--Page content goes here, fixed elements go above the all elements class-->
-                
+                <?php
+					include_once 'dataconfig/dataprovider.php';
+					$id_LoaiBaiHoc = $_GET["lsid"];
+					$sql_tieude = "select lbh_LoaiBaiHoc from loaibaihoc where lbh_ID = $id_LoaiBaiHoc";
+					$result = DataProvider::GetRows($sql_tieude);
+					$tieude;
+					foreach ($result as $value) 
+					{
+						$tieude = $value;
+					}
+				?>
                 <div class="container heading-style-5">
-                    <h4 class="heading-title">Phát âm theo câu</h4>
+                    <h4 class="heading-title"><?php echo $tieude[0]; ?></h4>
                     <i class="fa fa-th heading-icon"></i>
                     <div class="line bg-black"></div>
                     <p class="heading-subtitle">
