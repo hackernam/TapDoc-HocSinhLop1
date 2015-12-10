c54a5b9cb426555c249393d94349af3a1b18a945
hackernam <hackernamadmin@gmail.com>
2015-12-10 10:27:41 +0700

    Cập nhật database

--- Database/tapdoc.sql
+++ Database/tapdoc.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 09, 2015 at 03:30 PM
+-- Generation Time: Dec 10, 2015 at 04:27 AM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -40,12 +40,12 @@ CREATE TABLE IF NOT EXISTS `am` (
 --
 
 INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES
-(1, 'a', 'upload\\GhiAm\\Am\\a.mp3', 2),
-(2, 'á', 'upload\\GhiAm\\Am\\a.mp3', 2),
-(3, 'à', 'upload\\GhiAm\\Am\\a.mp3', 2),
-(4, 'ạ', 'sdfsdf', 2),
-(5, 'ã', 'fdgd', 2),
-(6, 'ả', 'dgdfg', 2);
+(1, 'a', 'upload/GhiAm/Am/a.wav', 2),
+(2, 'á', 'upload/GhiAm/Am/a sac.wav', 2),
+(3, 'à', 'upload/GhiAm/Am/a huyen.wav', 2),
+(4, 'ạ', 'upload/GhiAm/Am/a nang.wav', 2),
+(5, 'ã', 'upload/GhiAm/Am/a nga.wav', 2),
+(6, 'ả', 'upload/GhiAm/Am/a hoi.wav', 2);
 
 -- --------------------------------------------------------
 
@@ -69,9 +69,9 @@ CREATE TABLE IF NOT EXISTS `baihoc` (
 --
 
 INSERT INTO `baihoc` (`bh_ID`, `bh_TenBaiHoc`, `bh_LoaiBaiHoc`, `bh_NgayTao`, `bh_HinhDaiDien`, `bh_SoLanChon`) VALUES
-(1, 'Bài 1', 2, '2015-12-04 00:00:00', 'upload\\HinhAnh\\doccau.png', 10),
-(2, 'Âm "a"', 1, '2015-12-08 00:00:00', 'upload\\HinhAnh\\Am\\bangchucai.png', 11),
-(3, 'Hình Ảnh 1', 4, '2015-12-08 00:00:00', 'upload\\HinhAnh\\HinhAnh\\bangchucai.png', 12);
+(1, 'Bài 1', 2, '2015-12-04 00:00:00', 'upload/HinhAnh/doccau.png', 10),
+(2, 'Âm "a"', 1, '2015-12-08 00:00:00', 'upload/HinhAnh/Am/bangchucai.png', 11),
+(3, 'Hình Ảnh 1', 4, '2015-12-08 00:00:00', 'upload/HinhAnh/HinhAnh/bangchucai.png', 12);
 
 -- --------------------------------------------------------
 
@@ -182,7 +182,7 @@ CREATE TABLE IF NOT EXISTS `taikhoan` (
   `tk_MatKhau` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`tk_ID`),
   KEY `fk_taikhoan_gioitinh_idx` (`tk_GioiTinh`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=10 ;
 
 --
 -- Dumping data for table `taikhoan`
@@ -192,7 +192,9 @@ INSERT INTO `taikhoan` (`tk_ID`, `tk_HoTen`, `tk_GioiTinh`, `tk_TenDangNhap`, `t
 (1, 'ABC', 1, 'test', '123'),
 (3, 'asdasfds', 1, 'test123', '123456'),
 (6, 'Hoc Sinh', 1, 'hocsinh123', '123456'),
-(7, 'fghdgdfg', 1, 'testasdfs', 'asfdfsdg');
+(7, 'fghdgdfg', 1, 'testasdfs', 'asfdfsdg'),
+(8, 'sdfsdf', 1, 'abc1234', '1234567'),
+(9, 'sdfgdsgdf', 2, 'hoangnam123', '1234567');
 
 -- --------------------------------------------------------
 
@@ -214,16 +216,16 @@ CREATE TABLE IF NOT EXISTS `tu` (
 --
 
 INSERT INTO `tu` (`t_ID`, `t_NoiDung`, `t_DuongDanGhiAm`, `t_BaiHoc`) VALUES
-(1, 'Con heo', 'upload\\GhiAm\\Tu\\con heo.mp3', 1),
-(2, 'Con bò', 'upload\\GhiAm\\Tu\\con bo.mp3', 1),
-(3, 'Con cá', 'upload\\GhiAm\\Tu\\con ca.mp3', 1),
-(4, 'Con chuột', 'upload\\GhiAm\\Tu\\con chuot.mp3', 1),
-(5, 'Con gấu', 'upload\\GhiAm\\Tu\\con gau.mp3', 1),
-(6, 'Con khỉ', 'upload\\GhiAm\\Tu\\con khi.mp3', 1),
-(7, 'Con mèo', 'upload\\GhiAm\\Tu\\con meo.mp3', 1),
-(8, 'Con ngựa', 'upload\\GhiAm\\Tu\\con ngua.mp3', 1),
-(9, 'Con trâu', 'upload\\GhiAm\\Tu\\con trau.mp3', 1),
-(10, 'Con voi', 'upload\\GhiAm\\Tu\\con voi.mp3', 1);
+(1, 'Con heo', 'upload/GhiAm/Tu/con heo.mp3', 1),
+(2, 'Con bò', 'upload/GhiAm/Tu/con bo.mp3', 1),
+(3, 'Con cá', 'upload/GhiAm/Tu/con ca.mp3', 1),
+(4, 'Con chuột', 'upload/GhiAm/Tu/con chuot.mp3', 1),
+(5, 'Con gấu', 'upload/GhiAm/Tu/con gau.mp3', 1),
+(6, 'Con khỉ', 'upload/GhiAm/Tu/con khi.mp3', 1),
+(7, 'Con mèo', 'upload/GhiAm/Tu/con meo.mp3', 1),
+(8, 'Con ngựa', 'upload/GhiAm/Tu/con ngua.mp3', 1),
+(9, 'Con trâu', 'upload/GhiAm/Tu/con trau.mp3', 1),
+(10, 'Con voi', 'upload/GhiAm/Tu/con voi.mp3', 1);
 
 --
 -- Constraints for dumped tables
