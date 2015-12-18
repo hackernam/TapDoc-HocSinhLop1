d23e5e0a9dfdbeb168ecff873b2c7dde129c6999
xuandatbr <txdat92@gmail.com>
2015-12-18 12:17:10 +0700

    update 2
    sua lai them bai hoc

---
+++ Source-code/dataconfig/dataconfig/dataprovider.php
@@ -0,0 +1,88 @@
+<?php 
+	define("HOSTNAME", "localhost");
+	define("USERNAME", "root");
+	define("PASSWORD", "");
+	define("DATABASE", "tapdoc");
+
+class DataProvider  
+{ 
+
+	
+  public static function ExecuteQuery($sql) 
+  { 
+    $connection = mysqli_connect(HOSTNAME,USERNAME,PASSWORD,DATABASE) or 
+      	die ("couldn't connect to localhost"); 
+ 
+         
+    mysqli_query($connection,"set names 'utf8'"); 
+     
+    $result = mysqli_query($connection,$sql); 
+     
+    mysqli_close($connection); 
+     
+    return $result; 
+  }
+  
+  public static function ExecuteQueryArray($sql) 
+  { 
+    $connection = mysqli_connect(HOSTNAME,USERNAME,PASSWORD,DATABASE) or 
+      	die ("couldn't connect to localhost");
+ 
+         
+    mysqli_query($connection,"set names 'utf8'"); 
+	
+	$kq = mysqli_fetch_array($sql);
+     
+    mysqli_close($connection); 
+     
+    return $kq; 
+  }  
+  
+  public static function GetOneRow($sql)
+  {
+    $connection = mysqli_connect(HOSTNAME,USERNAME,PASSWORD,DATABASE) or 
+      	die ("couldn't connect to localhost");
+         
+    mysqli_query($connection,"set names 'utf8'"); 
+     
+    $result = mysqli_query($connection,$sql); 
+	$row = mysqli_fetch_array($result);
+     
+    mysqli_close($connection); 
+     
+    return $row; 
+  }
+
+  public static function GetRows($sql)
+  {
+	$connection = mysqli_connect(HOSTNAME,USERNAME,PASSWORD,DATABASE) or 
+      	die ("couldn't connect to localhost"); 
+         
+    mysqli_query($connection,"set names 'utf8'"); 
+    
+    $result = mysqli_query($connection,$sql);
+	$i = 0;
+	while ($row = mysqli_fetch_array($result))
+	{
+		$returnresult[$i++] = $row;
+	}
+    mysqli_close($connection); 
+    return $returnresult; 
+	}
+
+	public static function NumRows($sql)
+	{
+		$connection = mysqli_connect(HOSTNAME,USERNAME,PASSWORD,DATABASE) or 
+      	die ("couldn't connect to localhost");
+         
+    mysqli_query($connection,"set names 'utf8'"); 
+     
+    $result = mysqli_query($connection,$sql); 
+	$row = mysqli_num_rows($result);
+     
+    mysqli_close($connection); 
+     
+    return $row; 
+	}	
+} 
+?> 
---
+++ Source-code/dataconfig/include/inc-loading.php
@@ -0,0 +1,9 @@
+<div id="preloader">
+	<div id="status">
+        <div class="preloader-logo"></div>
+        <h3 class="center-text">Xin chào</h3>
+        <p class="center-text smaller-text">
+            Chúng tôi đang tải nội dung ! Xin chờ trong giây lát !
+        </p>
+    </div>
+</div>
