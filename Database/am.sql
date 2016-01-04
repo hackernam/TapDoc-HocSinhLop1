04f321bee17598f077b7d328103b42e98d65aff6
Bui Viet Tien <bvtien04@gmail.com>
2016-01-04 08:15:23 +0700

    du lieu phan am

---
+++ Database/am.sql
@@ -0,0 +1,47 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(1, 'a', 'upload/GhiAm/Am/a.mp3', 2);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(2, 'á', 'upload/GhiAm/Am/á.wav', 2);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(3, 'à', 'upload/GhiAm/Am/à.wav', 2);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(4, 'ạ', 'upload/GhiAm/Am/ạ.wav', 2);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(5, 'ã', 'upload/GhiAm/Am/ã.wav', 2);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(6, 'ả', 'upload/GhiAm/Am/ả.wav', 2);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(7, 'b', 'upload/GhiAm/Am/b.mp3', 5);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(8, 'c', 'upload/GhiAm/Am/c.mp3', 5);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(9, 'd', 'upload/GhiAm/Am/d.mp3', 5);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(10, 'đ', 'upload/GhiAm/Am/đ.mp3', 5);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(11, 'e', 'upload/GhiAm/Am/e.mp3', 6);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(12, 'ê', 'upload/GhiAm/Am/ê.mp3', 6);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(15, 'â', 'upload/GhiAm/Am/â.mp3', 6);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(16, 'ă', 'upload/GhiAm/Am/ă.mp3', 6);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(17, 'g', 'upload/GhiAm/Am/g.mp3', 6);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(18, 'h', 'upload/GhiAm/Am/h.mp3', 7);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(19, 'i', 'upload/GhiAm/Am/i.mp3', 7);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(20, 'k', 'upload/GhiAm/Am/k.mp3', 7);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(21, 'l', 'upload/GhiAm/Am/l.mp3', 7);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(22, 'm', 'upload/GhiAm/Am/m.mp3', 7);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(23, 'n', 'upload/GhiAm/Am/n.mp3', 8);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(24, 'o', 'upload/GhiAm/Am/o.mp3', 8);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(25, 'ô', 'upload/GhiAm/Am/ô.mp3', 8);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(26, 'ơ', 'upload/GhiAm/Am/ơ.mp3', 8);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(27, 'p', 'upload/GhiAm/Am/p.mp3', 8);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(28, 'q', 'upload/GhiAm/Am/q.mp3', 9);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(29, 'r', 'upload/GhiAm/Am/r.mp3', 9);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(30, 's', 'upload/GhiAm/Am/s.mp3', 9);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(31, 't', 'upload/GhiAm/Am/t.mp3', 9);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(32, 'u', 'upload/GhiAm/Am/u.mp3', 9);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(35, 'ư', 'upload/GhiAm/Am/ư.mp3', 10);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(36, 'v', 'upload/GhiAm/Am/v.mp3', 10);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(37, 'x', 'upload/GhiAm/Am/x.mp3', 10);
+INSERT INTO `am` (`a_ID`, `a_NoiDung`, `a_DuongDanGhiAm`, `a_BaiHoc`) VALUES(38, 'y', 'upload/GhiAm/Am/y.mp3', 10);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
